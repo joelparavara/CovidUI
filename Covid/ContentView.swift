697284d5fad71bbfa024491875daf2fe677ed188
joelparavara <joelparavara@gmail.com>
2020-08-16 22:53:20 +0530

    Initial Commit

---
+++ Covid/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Covid
+//
+//  Created by Joel Thomson on 8/16/20.
+//  Copyright © 2020 Techcrus Labs. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
